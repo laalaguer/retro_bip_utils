688863db2c48cdc165fc9ae8992e93726a7bbc40
laalaguer <laalaguer@gmail.com>
2023-07-25 16:57:13 +0800

    add name

--- retro_bip_utils/eth_addr.py
+++ retro_bip_utils/eth_addr.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Emanuele Bellocchia
+# Copyright (c) 2020 Emanuele Bellocchia, 2023 A Byte Ahead
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
